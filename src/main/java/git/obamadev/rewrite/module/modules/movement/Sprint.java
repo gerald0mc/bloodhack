3619fb9c29abb1f8f1ee6c98dd81e68be6b85f7c
ollie <olliemullen33@gmail.com>
2020-08-04 20:47:37 +1000

    fixed :bug:

--- src/main/java/git/obamadev/rewrite/module/modules/movement/Sprint.java
+++ src/main/java/git/obamadev/rewrite/module/modules/movement/Sprint.java
@@ -4,23 +4,18 @@ import git.obamadev.rewrite.module.Category;
 import git.obamadev.rewrite.module.Module;
 
 public class Sprint extends Module {
-    public Sprint(){
+    public Sprint() {
         super("Sprint", Category.MOVEMENT);
     }
 
-    @Override
-    public void onDisable() {
-        if (mc.world != null) {
-            mc.player.setSprinting(false);
-        }
-    }
-
     public void onUpdate() {
-        try {
-            if (mc.gameSettings.keyBindForward.isKeyDown() && !(mc.player.collidedHorizontally)) {
-                mc.player.setSprinting(true);
+        if (this.isToggled()) {
+            try {
+                if (mc.gameSettings.keyBindForward.isKeyDown() && !(mc.player.collidedHorizontally)) {
+                    mc.player.setSprinting(true);
+                }
+            } catch (Exception ignored) {
             }
-        } catch (Exception ignored) {
         }
     }
 }
