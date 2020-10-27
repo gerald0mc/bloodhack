91a697af37aa0cbbc9e7e0fdc9185fd5e622901f
Nowilltolife <gihihoh@gmail.com>
2020-10-27 02:30:42 +0100

    Discord rpc update

--- src/main/java/dev/lors/bloodhack/module/BloodModules/misc/DiscordRPC.java
+++ src/main/java/dev/lors/bloodhack/module/BloodModules/misc/DiscordRPC.java
@@ -3,8 +3,11 @@ package dev.lors.bloodhack.module.BloodModules.misc;
 import dev.lors.bloodhack.managers.DiscordManager;
 import dev.lors.bloodhack.module.Category;
 import dev.lors.bloodhack.module.Module;
+import net.minecraft.client.gui.GuiMultiplayer;
+import net.minecraft.client.gui.GuiWorldSelection;
 
 public class DiscordRPC extends Module {
+    DiscordManager manager = new DiscordManager();
     public DiscordRPC() {
         super("DiscordRPC", Category.MISC);
     }
